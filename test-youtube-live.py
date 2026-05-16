8cdff323060e42957809ee06c34fc50188e9adb1
VisualPlugin <aboshadi.n.ahmed@gmail.com>
2026-05-16 05:11:31 +0000

    Update and rename test-youtube.py to test-youtube-live.py

--- test-youtube-live.py
+++ test-youtube-live.py
@@ -4,27 +4,22 @@ import requests
 import json
 import re
 
-tokens = set()
+lines = set()
 while True:
     text = input()
     if not text:
         break
+    lines.add(text)
+
+for text in lines:
     match = re.search(r'[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}', text)
     if not match:
         continue
-    tokens.add(match.group(0))
-
-for token in tokens:
-    split = token.split('_')
-    if len(split) != 3:
-        continue
-
+    token = match.group(0)
     output = subprocess.run(
         f'ffmpeg -f lavfi -i color=black -c copy -f flv rtmp://a.rtmp.youtube.com/live2/{token}',
         stderr=subprocess.PIPE,
     ).stderr
-
     if b'Function not implemented' not in output:
         continue
-
     print(token)
